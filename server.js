d7ae65ca8d3f4b001e496f157d524428f8b49c75
seatangle77 <seatangle@outlook.com>
2025-06-06 16:04:54 +0800

    提交

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-// server.js
-import wsServer from 'y-websocket'
-const { setupWSConnection } = wsServer
 import * as http from 'http'
-import WebSocket from 'ws'
+import { setupWSConnection } from 'y-websocket/bin/utils.js'
+import WebSocket, { WebSocketServer } from 'ws'
 
-const server = http.createServer()
-const wss = new WebSocket.Server({ server })
+const port = process.env.PORT || 1234
+const host = '0.0.0.0'
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200)
+  res.end('Yjs WebSocket Server')
+})
+
+const wss = new WebSocketServer({ server })
 
 wss.on('connection', (conn, req) => {
   setupWSConnection(conn, req)
 })
 
-server.listen(1234, () => {
-  console.log('✅ Yjs WebSocket server running at ws://localhost:1234')
+server.listen(port, host, () => {
+  console.log(`✅ Yjs WebSocket server running at http://${host}:${port}`)
 })
