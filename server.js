b3b2d799f187037c2d42f8be441a82485c9ada13
seatangle77 <seatangle@outlook.com>
2025-06-06 16:44:00 +0800

    修改

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import * as http from 'http'
-import { setupWSConnection } from 'y-websocket/bin/utils.js'
+import { setupWSConnection } from 'y-websocket'
 import WebSocket, { WebSocketServer } from 'ws'
 
 const port = process.env.PORT || 1234
